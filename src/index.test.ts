e12fcb37c8539618972cd70b8f4e213e40cd9558
Kjell-Morten <post@kjellmorten.no>
2019-07-07 20:35:40 +0200

    Use .test.ts suffix

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,40 +6,46 @@ import mapAny = require('.')
 
 const arr = [{}, {}, {}]
 const obj = {}
-const setId: mapAny.Functor<{} | null | undefined> = (obj, index) => ({ ...obj, id: index })
-const markLast: mapAny.Functor<{}> = (obj, index, array) => ({ ...obj, last: array && index === array.length - 1 })
+const setId: mapAny.Functor<{} | null | undefined> = (obj, index) => ({
+  ...obj,
+  id: index
+})
+const markLast: mapAny.Functor<{}> = (obj, index, array) => ({
+  ...obj,
+  last: array && index === array.length - 1
+})
 
 // Tests
 
-test('should map array', (t) => {
+test('should map array', t => {
   const expected = [{ id: 0 }, { id: 1 }, { id: 2 }]
   const ret = mapAny(setId, arr)
 
   t.deepEqual(ret, expected)
 })
 
-test('should map object', (t) => {
+test('should map object', t => {
   const expected = { id: 0 }
   const ret = mapAny(setId, obj)
 
   t.deepEqual(ret, expected)
 })
 
-test('should map null', (t) => {
+test('should map null', t => {
   const expected = { id: 0 }
   const ret = mapAny(setId, null)
 
   t.deepEqual(ret, expected)
 })
 
-test('should map undefined', (t) => {
+test('should map undefined', t => {
   const expected = { id: 0 }
   const ret = mapAny(setId, undefined)
 
   t.deepEqual(ret, expected)
 })
 
-test('should provide original array', (t) => {
+test('should provide original array', t => {
   const expectedArr = [{ last: false }, { last: false }, { last: true }]
   const expectedObj = { last: true }
 
@@ -50,7 +56,7 @@ test('should provide original array', (t) => {
   t.deepEqual(retObj, expectedObj)
 })
 
-test('should be curried', (t) => {
+test('should be curried', t => {
   const expectedArr = [{ id: 0 }, { id: 1 }, { id: 2 }]
   const expectedObj = { id: 0 }
   const map = mapAny(setId)
@@ -59,14 +65,14 @@ test('should be curried', (t) => {
   t.deepEqual(map(obj), expectedObj)
 })
 
-test('should map undefined curried', (t) => {
+test('should map undefined curried', t => {
   const expected = { id: 0 }
   const ret = mapAny(setId)(undefined)
 
   t.deepEqual(ret, expected)
 })
 
-test('should not throw when given undefined', (t) => {
+test('should not throw when given undefined', t => {
   const expected = { id: 0 }
 
   const map = mapAny(setId)
--- src/index.ts
+++ src/index.ts
@@ -11,25 +11,27 @@ namespace mapAny {
 const isMappable = (obj: any): obj is mapAny.Mappable<any, any> =>
   typeof obj === 'object' && obj !== null && typeof obj.map === 'function'
 
-function map<T = any, U = any> (cb: mapAny.Functor<T, U>, mapee: T | T[]): U | U[] {
-  return (isMappable(mapee))
-    ? mapee.map(cb)
-    : cb(mapee as T, 0, [mapee as T])
+function map<T = any, U = any>(
+  cb: mapAny.Functor<T, U>,
+  mapee: T | T[]
+): U | U[] {
+  return isMappable(mapee) ? mapee.map(cb) : cb(mapee as T, 0, [mapee as T])
 }
 
-function mapAny<T = any, U = any> (cb: mapAny.Functor<T, U>): (mapee: T) => U
-function mapAny<T = any, U = any> (cb: mapAny.Functor<T, U>): (mapee: T[]) => U[]
-function mapAny<T = any, U = any> (cb: mapAny.Functor<T, U>, mapee: T): U
-function mapAny<T = any, U = any> (cb: mapAny.Functor<T, U>, mapee: T[]): U[]
-function mapAny<T = any, U = any> (cb: mapAny.Functor<T, U>, mapee?: T | T[]): U | U[] | ((mapee: T | T[]) => U | U[]) {
+function mapAny<T = any, U = any>(cb: mapAny.Functor<T, U>): (mapee: T) => U
+function mapAny<T = any, U = any>(cb: mapAny.Functor<T, U>): (mapee: T[]) => U[]
+function mapAny<T = any, U = any>(cb: mapAny.Functor<T, U>, mapee: T): U
+function mapAny<T = any, U = any>(cb: mapAny.Functor<T, U>, mapee: T[]): U[]
+function mapAny<T = any, U = any>(
+  cb: mapAny.Functor<T, U>,
+  mapee?: T | T[]
+): U | U[] | ((mapee: T | T[]) => U | U[]) {
   const argCount = arguments.length
-  function isUnary<V> (_arg?: V): _arg is undefined {
+  function isUnary<V>(_arg?: V): _arg is undefined {
     return argCount === 1 // This function is basically just for typing `mapee` correctly
   }
 
-  return (isUnary(mapee))
-    ? (mapee: any) => map(cb, mapee)
-    : map(cb, mapee)
+  return isUnary(mapee) ? (mapee: any) => map(cb, mapee) : map(cb, mapee)
 }
 
 export = mapAny
